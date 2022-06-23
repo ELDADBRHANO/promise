c4659c7a6b5e9a09bafa22de345ec81860b24d48
ELDADBRHANO <ebrhano@gmail.com>
2022-06-23 13:54:59 +0300

    commit-second

--- main.js
+++ main.js
@@ -8,13 +8,15 @@ function getPromise() {
     }
   });
 }
-getPromise()
-  .then((data) => {
-    my_p.innerText = data;
-  })
-  .catch((err) => {
-    my_p.innerText = err;
-  });
+function printPromise() {
+  getPromise()
+    .then((data) => {
+      my_p.innerText = data;
+    })
+    .catch((err) => {
+      my_p.innerText = err;
+    });
+}
 
 function getModolue(num1, num2) {
   return new Promise((resolve, reject) => {
@@ -25,13 +27,15 @@ function getModolue(num1, num2) {
     }
   });
 }
-getModolue(5, 10)
-  .then((data) => {
-    my_text.innerText = data;
-  })
-  .catch((err) => {
-    my_text.innerText = err;
-  });
+function printModolue() {
+  getModolue(5, 10)
+    .then((data) => {
+      my_text.innerText = data;
+    })
+    .catch((err) => {
+      my_text.innerText = err;
+    });
+}
 
 function checkIfNumIsInArray(num, arrayNumbers) {
   return new Promise((resolve, reject) => {
@@ -43,10 +47,42 @@ function checkIfNumIsInArray(num, arrayNumbers) {
     }
   });
 }
-checkIfNumIsInArray(6, [6, 7, 8, 12, 4, 3])
-  .then((answer) => {
-    demo_text.innerText = answer;
-  })
-  .catch((err) => {
-    demo_text.innerText = err;
-  });
+function printRes() {
+  checkIfNumIsInArray(6, [6, 7, 8, 12, 4, 3])
+    .then((answer) => {
+      demo_text.innerText = answer;
+    })
+    .catch((err) => {
+      demo_text.innerText = err;
+    });
+}
+
+function playGetSeif5() {
+  async function getSeif5() {
+    try {
+      btnGetSeif5.disabled = true;
+      await checkIfNumIsInArray(6, [6, 7, 8, 12, 4, 3]).then((data) => {
+        console.log(data);
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      btnGetSeif5.disabled = false;
+    }
+  }
+  getSeif5();
+}
+
+function btnCallUrl() {
+  async function callUrl() {
+    try {
+      await fetch("https:/moviesmern.herokuapp.com/movies/all").then((data) => {
+        console.log(data);
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+    }
+  }
+  callUrl();
+}
